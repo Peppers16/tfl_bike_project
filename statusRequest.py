f90b5cddb23e73ae0d9e8c7d36997b0629a33749
Josh <joshdh81691@gmail.com>
2019-10-27 16:57:15 +0000

    Functionality to format and append data to a CSV file.

--- statusRequest.py
+++ statusRequest.py
@@ -8,6 +8,9 @@ We will finally set up a script that periodically performs this action (e.g. eve
 """
 
 import requests
+import os.path
+import csv
+import datetime
 
 credentials_file = 'apiCredentials.txt'
 
@@ -44,8 +47,47 @@ def request_meta_severitycodes():
 
 def request_tube_status():
     """Request status for tube, DLR and Overground
-    we could also ask for national rail, river bus and bus services"""
+    we could also ask for national rail, river bus and bus services, but leaving out for now"""
     tube_status_url = 'https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr/Status'
     response = requests.get(tube_status_url, params=credentials)
     tube_status_json = response.json()
     return tube_status_json
+
+
+def create_csv(csv_out, headers):
+    if csv_out[-4:] != '.csv':
+        raise ValueError('Expecting a csv_out argument ending with ".csv"')
+    if os.path.isfile(csv_out):
+        print(csv_out + ' already exists. File will not be overwritten')
+        return
+    if type(headers) is not list:
+        raise ValueError('Expecting a list of headers as the headers argument')
+
+    f = open(csv_out, 'x', newline='')  # x to create file: error if it already exists
+    writer = csv.writer(f)
+    writer.writerow(headers)
+    f.close()
+
+
+def append_to_csv(csvfile, row_items):
+    with open(csvfile, 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(row_items)
+
+
+def extract_status_row(timestamp, line_status):
+    """Take a single linestatus from the status json and extracts data ready for a csv row"""
+    return [timestamp, line_status['modeName'], line_status['id'], line_status['name']
+            , '||'.join([str(status['statusSeverity']) for status in line_status['lineStatuses']])
+            , '||'.join([status['statusSeverityDescription'] for status in line_status['lineStatuses']])
+            ]
+
+
+def main():
+    csv_file = 'data/statuslog.csv'
+    create_csv(csv_file, ['timestamp', 'modeName', 'lineId', 'lineName', 'statusSeverity', 'severityDesc'])
+    request_time = datetime.datetime.now()
+    status_json = request_tube_status()
+    for line in status_json:
+        row_items = extract_status_row(request_time, line)
+        append_to_csv(csv_file, row_items)
--- test_statusRequests.py
+++ test_statusRequests.py
@@ -1,6 +1,5 @@
 from statusRequest import *
 
-
 def test_read_api_credentials():
     read_api_credentials('apiCredentials.txt')
     assert credentials['app_id'] != ''
@@ -23,3 +22,15 @@ def test_request_tube_status():
     for line in tube_status:
         assert line['modeName'] in ['tube', 'overground', 'dlr']
 
+
+def test_create_and_append_csv():
+    create_csv('test.csv', ['test1', 'test2'])
+    assert os.path.isfile('test.csv')
+    append_to_csv('test.csv', ['foo', 'bar'])
+    os.remove('test.csv')
+
+
+def test_extract_status_row():
+    statuses = request_tube_status()
+    statusrow = extract_status_row(datetime.datetime.now(), statuses[0])
+    assert len(statusrow) > 1
